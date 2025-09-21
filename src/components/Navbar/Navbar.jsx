6a490e0ed971043a90323e36723470c1a882d66e
unknown <mugadzap@gmail.com>
2025-09-21 11:04:59 +0100

    search icon showing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-left'>
-         <div className='navbar-right'>
-            <img src={logo} alt='logo '/>
-         </div>
-
+        <img src={logo} alt='logo '/>
+        <ul>
+          <li>Home</li>        
+          <li>Tv Shows</li> 
+          <li>Movies</li> 
+          <li>New & Popular</li> 
+          <li>My List</li> 
+          <li>Browse by Languages</li> 
+        </ul>
       </div>
+       <div className='navbar-right'>
+         <img src={search_icon} alt='search '/>
+         </div>
     </div>
   )
 }
